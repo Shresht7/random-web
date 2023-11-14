c5f3ae38529f9e4ea89f2bf4c87dcf6c21033e77
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-14 20:23:48 +0530

    Create helper function to generate random numbers

--- pages/numbers/script.js
+++ pages/numbers/script.js
@@ -1,3 +1,15 @@
+// DOM Elements
 const result = /* @type HTMLParagraphElement  */ (document.getElementById('result'))
 
-result.innerHTML = Math.ceil(Math.random() * 1000)
+/**
+ * Generate a random number between min and max
+ * @param {number} min Lower number
+ * @param {number} max Higher number
+ * @returns Random number between min and max
+ */
+function generateRandomNumber(min = 0, max = 1000) {
+    return min + Math.floor(Math.random() * max)
+}
+
+// Initialize a random result
+result.innerHTML = generateRandomNumber()
