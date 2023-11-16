80e3849f735270ce0dec52acc4bba7e7a8686b89
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-16 18:26:33 +0530

    Rig up min and max input options

--- pages/numbers/script.js
+++ pages/numbers/script.js
@@ -7,10 +7,23 @@ const button = /** @type HTMLButtonElement */ (document.getElementById('randomiz
 const minInput = /** @type HTMLInputElement */ (document.getElementById('min'))
 const maxInput = /** @type HTMLInputElement */ (document.getElementById('max'))
 
+// State
+let min = +minInput.value
+let max = +maxInput.value
+let number = generateRandomNumber(min, max)
+
 // Initialize a random result
-result.innerHTML = generateRandomNumber()
+result.innerHTML = number
 
 // Register an on-click event listener to regenerate the number when the button is pressed
 button.addEventListener('click', () => {
-    result.innerHTML = generateRandomNumber()
+    number = generateRandomNumber(min, max)
+    result.innerHTML = number
+})
+
+minInput.addEventListener('change', (e) => {
+    min = +e.target.value
+})
+maxInput.addEventListener('change', (e) => {
+    max = +e.target.value
 })
