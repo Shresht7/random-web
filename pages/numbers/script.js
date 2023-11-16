b9a6d5f6b96c2931bba8b0e5aa98bfcaab8210bf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-16 18:37:22 +0530

    Make use of the URLState to manage state

--- pages/numbers/script.js
+++ pages/numbers/script.js
@@ -2,19 +2,17 @@
 import { URLState } from "../../modules/classes/URLState.js";
 import { generateRandomNumber } from "../../modules/helpers/generateRandomNumbers.js"
 
-// DOM Elements
+// DOM Elements - Result and Randomize Button
 const result = /** @type HTMLParagraphElement  */ (document.getElementById('result'))
 const button = /** @type HTMLButtonElement */ (document.getElementById('randomize'))
-const minInput = /** @type HTMLInputElement */ (document.getElementById('min'))
-const maxInput = /** @type HTMLInputElement */ (document.getElementById('max'))
-
-// State
-let min = +minInput.value
-let max = +maxInput.value
-let number = generateRandomNumber(min, max)
 
 // URL State Manager
 const state = new URLState()
+
+// State
+let min = +state.get('min')
+let max = +state.get('max')
+let number = generateRandomNumber(min, max)
 
 // Initialize a random result
 result.innerHTML = number
@@ -25,15 +23,22 @@ button.addEventListener('click', () => {
     result.innerHTML = number
 })
 
+// DOM Elements - Min and Max Options
+const minInput = /** @type HTMLInputElement */ (document.getElementById('min'))
+const maxInput = /** @type HTMLInputElement */ (document.getElementById('max'))
+
+// Initialize the input values
+minInput.value = min
+maxInput.value = max
+
+// Register on change event handlers to update the state
 minInput.addEventListener('change', (e) => {
     min = +e.target.value
+    state.set('min', min)
+    state.push()
 })
 maxInput.addEventListener('change', (e) => {
     max = +e.target.value
+    state.set('max', max)
+    state.push()
 })
-
-new MutationObserver((records) => {
-    records.forEach(record => {
-        state.setState('number', record.target.innerText)
-    })
-}).observe(result, { childList: true })
