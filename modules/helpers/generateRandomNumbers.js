d7787cce94caa991ee2095b68060ded04b365c0e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-16 18:26:20 +0530

    Fix bad mathematics

--- modules/helpers/generateRandomNumbers.js
+++ modules/helpers/generateRandomNumbers.js
@@ -5,5 +5,7 @@
  * @returns Random number between min and max
  */
 export function generateRandomNumber(min = 0, max = 1000) {
-    return min + Math.floor(Math.random() * max)
+    min = min || 0
+    max = max || 1000
+    return min + Math.floor(Math.random() * (max - min))
 }
