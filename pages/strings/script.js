3143f59ff2f1f1b572cd1ac659995ad02e797aa8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-17 21:54:32 +0530

    Setup URL State Management

--- pages/strings/script.js
+++ pages/strings/script.js
@@ -1,14 +1,24 @@
 // Library
+import { URLState } from "../../modules/classes/URLState.js";
 import { generateRandomString } from "../../modules/helpers/generateRandomString.js";
 
 // DOM Elements
 const result = /** @type HTMLParagraphElement  */ (document.getElementById('result'))
 const button = /** @type HTMLButtonElement */ (document.getElementById('randomize'))
 
+// URL State Manager
+const state = new URLState()
+
+// State
+let charset = state.get('charset') || 'ALPHANUMERIC'
+let length = +state.get('length') || 16
+let string = generateRandomString(charset, length)
+
 // Initialize a random string
-result.innerText = generateRandomString()
+result.innerText = string
 
 // Register an on-click event listener to regenerate the string when the button is pressed
 button.addEventListener('click', () => {
-    result.innerText = generateRandomString()
+    string = generateRandomString(charset, length)
+    result.innerText = string
 })
