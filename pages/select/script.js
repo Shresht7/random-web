eb995165e5909c3ba7518a1ba42b44879ed6f8ce
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-22 14:52:09 +0530

    Scaffold select

---
+++ pages/select/script.js
@@ -0,0 +1,33 @@
+//@ts-check
+
+// DOM Elements
+const button = /** @type HTMLButtonElement */ (document.getElementById('randomize'))
+const input = /** @type HTMLInputElement */ (document.getElementById('input'))
+const list = /** @type HTMLDivElement */ (document.getElementById('list'))
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        let text = input.value
+        addToList(text)
+        input.value = ""
+    }
+})
+
+function addToList(item) {
+    const div = document.createElement('div')
+    div.classList.add('list-item')
+    const text = document.createElement('p')
+    text.innerText = item
+    div.appendChild(text)
+    list.appendChild(div)
+}
+
+// Register a on-click event listener to select one of the element from the list
+button.addEventListener('click', () => {
+    for (const child of list.children) {
+        child.classList.remove('selected')
+    }
+    const idx = Math.floor(Math.random() * list.children.length)
+    const selection = list.children.item(idx)
+    selection?.classList.add('selected')
+})
